c824a48c2e6eef17d3c8c08f45294c68914042bf
239573049@qq.com <239573049@qq.com>
2022-01-18 21:08:48 +0800

    删除老版本权限枚举

--- Core/Entitys/DbEnums.cs
+++ Core/Entitys/DbEnums.cs
@@ -15,22 +15,4 @@
         /// </summary>
         Freeze
     }
-    /// <summary>
-    /// 单权限(加权限枚举的时候按照从小到大去配置权限0的权限最大)
-    /// </summary>
-    public enum PowerEnum
-    {
-        /// <summary>
-        /// 管理员
-        /// </summary>
-        Admin,
-        /// <summary>
-        /// 审核
-        /// </summary>
-        Audit,
-        /// <summary>
-        /// 用户
-        /// </summary>
-        User
-    }
 }
