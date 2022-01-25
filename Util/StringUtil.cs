2f9b275aec333cd3f6f1f748612a29e3ef0c4929
239573049@qq.com <239573049@qq.com>
2022-01-25 23:02:01 +0800

    修改随机字符串方法

--- Util/StringUtil.cs
+++ Util/StringUtil.cs
@@ -1,7 +1,10 @@
-﻿namespace Util
+﻿using System;
+
+namespace Util
 {
     public class StringUtil
     {
+        private static readonly string str= "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890";
         /// <summary>
         /// 获取随机字符串
         /// </summary>
@@ -10,10 +13,10 @@
         public static string GetString(int s)
         {
             string data = "";
-            var chats = new char[] {'q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m','Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M','1','2','3','4','5','6','7','8','9','0' };
+            var random = new Random();
             for (int i = 0; i < s; i++)
             {
-                data+=chats[new Random().Next(chats.Length-1)];
+                data += str[random.Next(str.Length - 1)];
             }
             return data;
         }
@@ -29,10 +32,10 @@
             {
 
                 string data = "";
-                var chats = new char[] { 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
+                var random= new Random();
                 for (int i = 0; i < s; i++)
                 {
-                    data += chats[new Random().Next(chats.Length - 1)];
+                    data += str[random.Next(str.Length - 1)];
                 }
                 return data;
             });
